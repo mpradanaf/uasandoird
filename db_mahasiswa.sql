f1df06e9fd7d6631fc5fb535c3d94fdf7c563eb9
mpradanaf <46588734+mpradanaf@users.noreply.github.com>
2020-01-21 17:41:35 +0800

    Add files via upload

---
+++ db_mahasiswa.sql
@@ -0,0 +1,107 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.2
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Jan 17, 2020 at 07:31 PM
+-- Server version: 10.1.13-MariaDB
+-- PHP Version: 5.6.21
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_mahasiswa`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_admin`
+--
+
+CREATE TABLE `tb_admin` (
+  `id` int(11) NOT NULL,
+  `username` varchar(20) DEFAULT NULL,
+  `password` varchar(30) DEFAULT NULL,
+  `level` varchar(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tb_admin`
+--
+
+INSERT INTO `tb_admin` (`id`, `username`, `password`, `level`) VALUES
+(1, 'admin', '12345', 'admin'),
+(2, 'member', '12345', 'member');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_mahasiswa`
+--
+
+CREATE TABLE `tb_mahasiswa` (
+  `id` int(11) NOT NULL,
+  `npm` int(11) DEFAULT NULL,
+  `nama` varchar(20) DEFAULT NULL,
+  `tempat_lahir` varchar(20) DEFAULT NULL,
+  `tanggal_lahir` varchar(20) DEFAULT NULL,
+  `jenis_kelamin` varchar(12) DEFAULT NULL,
+  `agama` varchar(10) DEFAULT NULL,
+  `no_telp` varchar(13) DEFAULT NULL,
+  `alamat` varchar(30) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tb_mahasiswa`
+--
+
+INSERT INTO `tb_mahasiswa` (`id`, `npm`, `nama`, `tempat_lahir`, `tanggal_lahir`, `jenis_kelamin`, `agama`, `no_telp`, `alamat`) VALUES
+(2, 17630591, 'angga ', 'kotabaru', '30081997', 'laki', 'islam', '082154222605', 'Banjarbaru'),
+(5, 12345678, 'bambanggg', 'bjb', '11111991', 'laki', 'islam', '082121212', 'banjarmasin'),
+(6, 919191919, 'budi', 'hssjshbs', '12121111', 'ajjaja', 'snnsnz', '9818828228', 'majanan'),
+(9, 19212128, 'ali muhammad', 'sbbn', '11111991', 'laki', 'islam', '091882828', 'sui ulin'),
+(10, 717272727, 'sari rahayu', 'martapura', '11111811', 'perempuan', 'islam', '08271772727', 'martapura'),
+(11, 17634571, 'santi', 'banjarbaru', '12121996', 'perempuan', 'islam', '082154222605', 'jl.sungai ulin'),
+(12, 17643233, 'surya', 'banjarmasin', '12121995', 'laki', 'islam', '082154222605', 'Banjarmasin no.5');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tb_admin`
+--
+ALTER TABLE `tb_admin`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tb_mahasiswa`
+--
+ALTER TABLE `tb_mahasiswa`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tb_admin`
+--
+ALTER TABLE `tb_admin`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+--
+-- AUTO_INCREMENT for table `tb_mahasiswa`
+--
+ALTER TABLE `tb_mahasiswa`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
